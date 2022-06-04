a6e109e558f661dae0fe927f319639cf1abe926d
HyeJun <solar3070@naver.com>
2022-06-04 23:24:12 +0900

    220504

---
+++ lev1/수박수박수박수박수박수.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = ''
+    
+    for i in range(n):
+        if i % 2 == 0:
+            answer += '수'
+        elif i % 2 == 1:
+            answer += '박'
+        
+    return answer
