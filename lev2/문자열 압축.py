d1fa28631932977ec43ff77955bd0757f96c4921
HyeJun <solar3070@naver.com>
2022-06-22 02:59:22 +0900

    220622

--- lev2/문자열 압축.py
+++ lev2/문자열 압축.py
@@ -1,32 +1,21 @@
 def solution(s):
-    len_list = [len(s) + 1] * (len(s) + 1)
-    for i in range(1, len(s) + 1):
-        w = s[0:i]
-        same = count = 1
-        short = ""
+    min = len(s)
+    for i in range(1, len(s) // 2 + 1):
+        answer = ""
+        count = 1
+        before = s[0 : i]
         for j in range(i, len(s) + 1, i):
-            if w != s[j:j + i]:
-                same = 0
-
-            if same == 0:
-                if count == 1:
-                    short += w
-                else:
-                    short += str(count) + w
-                same = count = 1
-            else:
+            current = s[j : j + i]
+            if before == current:
                 count += 1
-            w = s[j:j + i]
-
-            if j + i >= len(s):
-                if j + i == len(s):
-                    if count == 1:
-                        short += w
-                    else:
-                        short += str(count) + w
-                elif j + i > len(s):
-                    short += s[j:]
-                len_list[i] = len(short)
-                break
-
-    return min(len_list)
+            else:
+                if count != 1:
+                    answer += str(count)
+                answer += before
+                if len(current) < len(before):
+                    answer += current  
+                before = current
+                count = 1
+        if len(answer) < min:
+            min = len(answer)  
+    return min
