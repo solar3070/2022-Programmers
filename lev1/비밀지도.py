2daf504c471bc93d27f0cbe773c3022931ec3fca
HyeJun <solar3070@naver.com>
2022-08-26 18:05:22 +0900

    220826

--- lev1/비밀지도.py
+++ lev1/비밀지도.py
@@ -1,19 +1,9 @@
-def binary(len, n):
-    answer = [0] * len
-    for i in range(len - 1, -1, -1):
-        if n == 0:
-            break
-        answer[i] = n % 2
-        n //= 2
-    return answer
-
 def solution(n, arr1, arr2):
     answer = []
     for i in range(n):
-        tmp = ""
-        map1 = binary(n, arr1[i])
-        map2 = binary(n, arr2[i])
-        for j in range(n):
-            tmp += " " if map1[j] == 0 and map2[j] == 0 else "#"  
+        tmp = str(bin(arr1[i]|arr2[i])[2:])
+        tmp = tmp.rjust(n, '0')
+        tmp = tmp.replace('1', '#')
+        tmp = tmp.replace('0', ' ')
         answer.append(tmp)
     return answer
