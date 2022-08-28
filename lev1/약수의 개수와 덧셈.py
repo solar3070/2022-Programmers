c6e82c0d4f97b0a4f99173b18de30800ce06675e
HyeJun <solar3070@naver.com>
2022-08-29 01:35:10 +0900

    Lv1 [약수의 개수와 덧셈] 두 번째 풀이

--- lev1/약수의 개수와 덧셈.py
+++ lev1/약수의 개수와 덧셈.py
@@ -2,13 +2,9 @@ def solution(left, right):
     answer = 0
     
     for n in range(left, right + 1):
-        total = 2
-        for i in range(2, n):
-            if n % i == 0:
-                total += 1
-        if total % 2 == 0 and n != 1:
-            answer += n
-        else:
+        if int(n ** 0.5) == n ** 0.5:
             answer -= n
+        else:
+            answer += n
     
     return answer
