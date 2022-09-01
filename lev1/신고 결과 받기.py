f29f72b182b220ee3959cb479359f84413f99bd8
HyeJun <solar3070@naver.com>
2022-09-01 12:56:55 +0900

    Lv1 [신고 결과 받기] 두 번째 풀이

--- lev1/신고 결과 받기.py
+++ lev1/신고 결과 받기.py
@@ -1,17 +1,12 @@
 def solution(id_list, report, k):
+    answer = [0] * len(id_list)
     dict_id = {id: 0 for id in id_list}
-    temp = list(set(report))
-    for r in temp:
+    
+    for r in set(report):
         dict_id[r.split()[1]] += 1
+        
+    for r in set(report):
+        if dict_id[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
     
-    report_user = []
-    for key, value in dict_id.items():
-        if value >= k:
-            report_user.append(key)
-
-    dict_id = {id: 0 for id in id_list}
-    for r in temp:
-        if r.split()[1] in report_user:
-            dict_id[r.split()[0]] += 1
-    
-    return list(dict_id.values())
+    return answer
